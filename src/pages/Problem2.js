4f7e5940bcd325813dba5ef8a4751da60e4c8a2c
Tobias Olausson <tobias.olausson@yh.nackademin.se>
2023-09-05 16:44:00 +0200

    date select works

--- src/pages/Problem2.js
+++ src/pages/Problem2.js
@@ -13,7 +13,13 @@ export function Problem2() {
     //Creating an array with names of the games we have data for. Using "Set" to remove duplicates.
     const games = [...new Set(gameDataArray.map((data) => data.game))]
 
-    //Function that in turn runs "setUserData" and sets that data to the games we want to show in the diagrams.
+    //Creating an array with all the dates we have some data on. Using "Set" to remove duplicates.
+    const availableDates = [...new Set(gameDataArray.map((data) => data.date))]
+    //Crating variables for the first and the last date available.
+    const minDate = availableDates[0];
+    const maxDate = availableDates[availableDates.length - 1]
+
+    //Function that in turn runs "setUserData" and sets that data to the games we want to show in the diagrams. The function is itself runned in the "checkboxChange()" function.
     //NOTE TO SELF: Try to make function work even if some games would miss data on dates. For instance data exist from an earlier date of one game than the other (probably seperate dates into other separate function).
     const activeGames = (games) => {
         let datasets = [];
@@ -23,8 +29,9 @@ export function Problem2() {
                 label: game,
                 data: [],
             }
-            gameDataArray.forEach((datapoint) => {
-                if (datapoint.game === game) {
+            gameDataArray.forEach((datapoint, i) => {
+                //Only runns if the data is from a game which checkbox is checked and only if the data is from within the timespan of selected dates.
+                if (datapoint.game === game && (datapoint.date >= document.getElementById("beginningDateInput").value) && (datapoint.date <= document.getElementById("endingDateInput").value)) {
                     object.data.push(datapoint.activeUsers)
                     newDates.push(datapoint.date)
                 }
@@ -38,17 +45,18 @@ export function Problem2() {
         })
     }
 
-    //This function is runned evertime someone changes one of the checkboxes.
+    //This function is runned every time someone changes one of the checkboxes, the min/max dates or first time "Problem 2"-page is started.
     const checkboxChange = () => {
         let checkboxArray = [];
         let form = document.getElementById("gamesCheckboxForm");
-        let checkboxes = form.getElementsByTagName("input");
+        let checkboxes = form.querySelectorAll('input[type=checkbox]');
 
         for (let i = 0; i < checkboxes.length; i++ ) {
             if (checkboxes[i].checked) {
                 checkboxArray.push(checkboxes[i].value)
             }
         }
+        //Runnes function with the selected games.
         activeGames(checkboxArray);
     }
 
@@ -60,27 +68,32 @@ export function Problem2() {
     }, [])
 
 
-    //The data of the games that the diagrams should show.
+    //A state with the data of the games that the diagrams should show. The "setUserData" is runned from "activeGames".
     const [userData, setUserData] = useState({
          labels: [],
          datasets: [],
      });
 
+
+    //State that by default is "false", if "true" a bar chart is showned instead of the line chart. Is changed by a button rendered at the end of the HTML bellow.
     const [showBarDiagram, setShowBarDiagram] = useState(false);
 
     return (
         <div>
             <h3>Problem 2: Dashboard frontend</h3>
-            <p>Please use the checkboxes to select the games you want to compare.</p>
+            <p>Please use the checkboxes to select the games you want to compare and use the date inputs to select min/max dates.</p>
             <form id="gamesCheckboxForm">
                 {games.map((game, i) => {
                     return <GameCheck game={game} key={i} checkboxChange={checkboxChange}/>
                 })}
+                <br/>
+                <input aria-label="Date input for starting date." id="beginningDateInput" type="date" min={minDate} max={maxDate} defaultValue={minDate} onChange={() =>{checkboxChange()}} ></input>
+                <input aria-label="Date input for ending date." id="endingDateInput" type="date" min={minDate} max={maxDate} defaultValue={maxDate} onChange={() =>{checkboxChange()}}></input>
             </form>
             <div id="diagramDiv">
             {!showBarDiagram ? <LineDiagram lineData={userData}/> : <BarDiagram barData={userData}/>}
             </div>
-            <button onClick={() => {setShowBarDiagram((prevState) => !prevState)}}>{!showBarDiagram ? "Show Bar Diagram" : "Show Line Diagram"}</button>
+            <button onClick={() => {setShowBarDiagram((prevState) => !prevState)}}>{!showBarDiagram ? "Show Bar Chart" : "Show Line Chart"}</button>
         </div>
     )
 }
