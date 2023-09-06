252656df8f2fb150e5824cafa5dfdf8d52dee65c
Tobias Olausson <tobias.olausson@yh.nackademin.se>
2023-09-06 13:14:54 +0200

    done with problem 3 (mvp)

---
+++ src/components/PuppyImage.js
@@ -0,0 +1,7 @@
+export default function PuppyImage({url,alt}) {
+    return (
+        <div>
+            <img src={url} alt={alt}/>
+        </div>
+    )
+}
--- src/components/PuppyRadio.js
+++ src/components/PuppyRadio.js
@@ -1,8 +1,10 @@
-export default function PuppyRadio({color}) {
+export default function PuppyRadio({color, activeRadio, setActiveRadio, setActivePage}) {
     return (
         <div>
             <label htmlFor={color}>{color}</label>
-            <input type="radio" id={color} name="color" />
+            <input type="radio" id={color} name="color" value={color} checked={color === activeRadio} onChange={() => {
+                setActiveRadio(color)
+                setActivePage(1)}}/>
         </div>
     )
 }
